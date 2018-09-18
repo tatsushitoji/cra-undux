05c37c283c85d7fdff12df68c416cb7323500f0a
ttoji <tatsushi.toji@gmail.com>
2018-09-18 12:02:56 +0900

    feat: optimize connector

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,11 +1,33 @@
 import * as React from 'react'
+import { compose, mapProps, withHandlers } from 'recompose';
 import { withStore } from '../../store'
 
-export const Counter = withStore(({ store }) => (
-  <div>
-    <p>count: {store.get('countValue')}</p>
-    <button onClick={() => store.set('countValue')(store.get('countValue') + 1)}>Increment</button>
-    <button onClick={() => store.set('countValue')(store.get('countValue') - 1)}>Decrement</button>
-    <button onClick={() => store.set('countValue')(0)}>Reset</button>
-  </div>
-))
+const Enhancer = compose(
+  withStore,
+  withHandlers({
+    onIncrement: ({ store }) => () => store.set('countValue')(store.get('countValue') + 1),
+    onDecrement: ({ store }) => () => store.set('countValue')(store.get('countValue') - 1),
+    onReset: ({ store }) => () => store.set('countValue')(0),
+  }),
+  mapProps(({ store, onIncrement, onReset, onDecrement }) => ({
+    onIncrement,
+    onDecrement,
+    onReset,
+    countValue: store.get('countValue'),
+  })),
+)
+
+export const Counter = Enhancer(({
+  onIncrement,
+  onDecrement,
+  onReset,
+  countValue,
+}) => (
+    <div>
+      <p>count: {countValue}</p>
+      <button onClick={onIncrement}>Increment</button>
+      <button onClick={onDecrement}>Decrement</button>
+      <button onClick={onReset}>Reset</button>
+    </div>
+  )
+)
